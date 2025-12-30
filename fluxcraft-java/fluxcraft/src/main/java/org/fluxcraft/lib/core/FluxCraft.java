fd331cf578b1b22d9f5726142cf319c6e19b76ff
andem20 <anderslindhardtmadsen@gmail.com>
2025-12-30 20:08:38 +0100

    add rust build for java

--- fluxcraft-java/fluxcraft/src/main/java/org/fluxcraft/lib/core/FluxCraft.java
+++ fluxcraft-java/fluxcraft/src/main/java/org/fluxcraft/lib/core/FluxCraft.java
@@ -1,15 +1,20 @@
 package org.fluxcraft.lib.core;
 
+import java.net.URISyntaxException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class FluxCraft {
     static {
         // FIXME
         String osName = System.getProperty("os.name");
         String extention = osName.equalsIgnoreCase("linux") ? "so" : "dll";
-        Path path = Paths.get("../../fluxcraft-rs/target/debug/libfluxcraft_jni." + extention).toAbsolutePath();
-        System.load(path.toString());
+        Path path;
+        try {
+            path = Path.of(ClassLoader.getSystemResource("libfluxcraft_jni." + extention).toURI()).toAbsolutePath();
+            System.load(path.toString());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
     public Pipeline load(String path) {
